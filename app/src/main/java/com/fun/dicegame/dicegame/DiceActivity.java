ae8b14aa76e86368c04f227c966e361b8bb35412
sserges <agounkeserge@gmail.com>
2018-12-12 07:02:28 +0000

    Roll dice action added

--- app/src/main/java/com/fun/dicegame/dicegame/DiceActivity.java
+++ app/src/main/java/com/fun/dicegame/dicegame/DiceActivity.java
@@ -1,4 +1,38 @@
 package com.fun.dicegame.dicegame;
 
-public class DiceActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+
+public class DiceActivity extends Activity implements View.OnClickListener {
+    private TextView textResult;
+    private int max;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+
+        max = getIntent().getIntExtra("max", 0);
+
+        TextView textTitle = findViewById(R.id.textTitle);
+        textTitle.setText(max + " sided dice");
+
+        textResult = findViewById(R.id.textResult);
+        textResult.setText("");
+
+        Button buttonRoll = findViewById(R.id.buttonRoll);
+        buttonRoll.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        SecureRandom random = new SecureRandom();
+        int result = random.nextInt(max) + 1;
+        textResult.setText(String.valueOf(result));
+    }
 }
--- app/src/main/java/com/fun/dicegame/dicegame/MainActivity.java
+++ app/src/main/java/com/fun/dicegame/dicegame/MainActivity.java
@@ -1,12 +1,36 @@
 package com.fun.dicegame.dicegame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DiceActivity.class);
+                intent.putExtra("max", 6);
+                startActivity(intent);
+            }
+        });
+
+        Button button20 = (Button) findViewById(R.id.button20);
+        button20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DiceActivity.class);
+                intent.putExtra("max", 20);
+                startActivity(intent);
+            }
+        });
     }
 }
